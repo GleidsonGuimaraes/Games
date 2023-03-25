8318d262aeda40b1876f83cf1a01872ac20dd35c
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2023-03-24 21:41:35 -0300

    verificando caracteres

--- js/jogoDaForca.js
+++ js/jogoDaForca.js
@@ -1,7 +1,7 @@
 const listWords = ["Torneira", "Comida", "Tomate", "Estanho", "Biscoito", "Chinchila", "Periquito", "Coruja", "Pescoço", "Boca", "Cadeira", "Recreio", "Jornal", "Revista", "Polícia", "Sapateiro", "Bicicleta", "Foguete"];
 const divWords = document.getElementById("palavra");
 
-let divStr;
+let divStr, array;
 
 function createDivStrings(){
     divStr = document.createElement("div");
@@ -15,21 +15,30 @@ function createDivStrings(){
     divStr.classList.add("w-5");
     divStr.classList.add("d-flex");
     divStr.classList.add("justify-content-center");
-    divStr.classList.add("text-uppercase");
     divStr.style.padding = "5px 0px 5px 0px";
     divWords.appendChild(divStr);
 }
 
 function caracteres(){
     let sort = parseInt(Math.random()*(listWords.length-1));
-    let array = listWords[sort];
+    array = listWords[sort].toUpperCase();
     for(let i=0; i<=array.length-1; i++){
         createDivStrings();
-        divStr.innerText = array[i];
+        divStr.setAttribute('id',`d${i}`);
     }
     console.log(sort, listWords[sort]);
 }
 
-caracteres();
-// createDivStrings();
-// console.log(divWords.innerHTML);
+function verificandoCaracteres(char){
+    for(let i=0; i<=array.length-1; i++){
+        if(char === array[i]){
+            document.getElementById(`d${i}`).innerText = char;
+        }
+    }
+}
+
+function iniciar(){
+    caracteres();
+}
+
+window.addEventListener('load', iniciar);
